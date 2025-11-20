44ea8bee04fb71fd3fdccdb9090264f1a56c27de
no-int <tkdduf2122@naver.com>
2025-11-20 22:25:48 +0900

    clone 2차

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,10 @@ class App extends StatelessWidget {
             horizontal: 40,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 80,
+                height: 50,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -41,9 +42,52 @@ class App extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Column(),
                 ],
-              )
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text("Total Balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withAlpha(200),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text('\$5 194 382',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 50,
+                        ),
+                        child: Text("Transfer",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
