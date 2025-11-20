54ed1c6f8fbe2e9443c1c11f0539dcba606c13e1
no-int <tkdduf2122@naver.com>
2025-11-20 23:09:08 +0900

    clone 2차 코드정리

--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,33 @@ void main() {
 }
 
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 50,
-              ),
+              SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text("Hey, Selena",
+                      Text(
+                        "Hey, Selena",
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w600
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
-                      Text("Welcom back",
+                      Text(
+                        "Welcom back",
                         style: TextStyle(
                           color: Colors.white.withAlpha(127),
                           fontSize: 18,
@@ -44,28 +41,24 @@ class App extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(
-                height: 40,
-              ),
-              Text("Total Balance",
+              SizedBox(height: 40),
+              Text(
+                "Total Balance",
                 style: TextStyle(
                   fontSize: 22,
                   color: Colors.white.withAlpha(200),
                 ),
               ),
-              SizedBox(
-                height: 5,
-              ),
-              Text('\$5 194 382',
+              SizedBox(height: 5),
+              Text(
+                '\$5 194 382',
                 style: TextStyle(
                   fontSize: 48,
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               Row(
                 children: [
                   Container(
@@ -74,16 +67,17 @@ class App extends StatelessWidget {
                       borderRadius: BorderRadius.circular(40),
                     ),
                     child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 20,
-                            horizontal: 50,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
                         ),
-                        child: Text("Transfer",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
+                      ),
                     ),
                   ),
                 ],
