04f00aed9b6dd68754062d2d43f207d560faa1a2
no-int <tkdduf2122@naver.com>
2025-11-21 17:22:57 +0900

    webtoon clone init

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,11 @@ void main() {
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  int counter = 0;
-
-  void onClicked() {
-    setState(() {
-      counter++;
-    });
-  }
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Total Count', style: TextStyle(fontSize: 30)),
-              Text('$counter', style: TextStyle(fontSize: 30)),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: Icon(Icons.add_box_rounded),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
