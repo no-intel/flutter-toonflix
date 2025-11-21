87e7e7881519c0e7f23c9a59f51630eb8a66eb0c
no-int <tkdduf2122@naver.com>
2025-11-21 14:07:29 +0900

    stateful widget 카운터

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Hey, Selena",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "Welcom back",
-                          style: TextStyle(
-                            color: Colors.white.withAlpha(127),
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withAlpha(200),
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  '\$5 194 382',
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: "Transfer",
-                      bgColor: Color(0xFFF1B33B),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: "Request",
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 80),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withAlpha(127),
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-                CurrencyCard(
-                  name: 'Euro',
-                  amount: '6 428',
-                  code: 'EUR',
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  offset: Offset(0, 0),
-                ),
-                SizedBox(height: 20),
-                CurrencyCard(
-                  name: 'Bitcoin',
-                  amount: '9 785',
-                  code: 'BTC',
-                  icon: Icons.currency_bitcoin_rounded,
-                  isInverted: true,
-                  offset: Offset(0, -20),
-                ),
-                CurrencyCard(
-                  name: 'Dollar',
-                  amount: '428',
-                  code: 'USD',
-                  icon: Icons.attach_money_outlined,
-                  isInverted: false,
-                  offset: Offset(0, -45),
-                ),
-              ],
-            ),
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Total Count', style: TextStyle(fontSize: 30)),
+              Text('$counter', style: TextStyle(fontSize: 30)),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: Icon(Icons.add_box_rounded),
+              ),
+            ],
           ),
         ),
       ),
